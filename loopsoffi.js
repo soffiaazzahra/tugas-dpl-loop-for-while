47f41c08358eb4dd3616519231800c85b9c3ba5e
soffiaazzahra <118706738+soffiaazzahra@users.noreply.github.com>
2022-11-22 15:01:34 +0700

    Tugas loop for & while

---
+++ loopsoffi.js
@@ -0,0 +1,12 @@
+let i = 0;
+while (i<10){
+    i++;
+        if(i%2==0) {
+            console.log(i , "adalah bilangan genap");
+    }
+}
+
+for(var s = 0 ; s<=10;s++){
+    if(s%2===0){
+        console.log(s ,"adalah bilangan genap");
+    }}
